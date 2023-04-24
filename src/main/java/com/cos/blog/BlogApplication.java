928deee6388bb5684ef529937b45d50716c7c6b5
Griotold <gotjd9773@naver.com>
2023-04-24 13:35:03 +0900

    프로젝트 실행해보기 Hello World

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -2,6 +2,7 @@ package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class BlogApplication {
