a714d64d8b8e5acdf9c95d4919fd480f4f7518c6
Griotold <gotjd9773@naver.com>
2023-04-25 16:58:55 +0900

    전체 회원 조회 + 페이징 처리

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,18 +4,36 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class DummyControllerTest {
 
     private final UserRepository userRepository;
+
+
+    // 한 페이지당 2건에 데이터를 리턴 받기
+    @GetMapping("dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        List<User> userList = pagingUser.getContent();
+        return userList;
+    }
+    @GetMapping("/dummy/users")
+    public List<User> userList() {
+        return userRepository.findAll();
+    }
 
     @GetMapping("/dummy/user/{id}")
     public User detail(@PathVariable Long id) {
